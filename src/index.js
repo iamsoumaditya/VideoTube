e14e690a8d4fa272363c96abcf467efa3f039cb7
iamsoumaditya <iamsoumadityaroy@gmail.com>
2025-10-26 15:47:43 +0530

    Change server starting comment

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 connectDB()
     .then(() => {
         app.listen(port, () => {
-          console.log(`Server is running on http://localhost:${port}`);
+          console.log(`Server is running 🏃‍➡️🏃‍♀️‍➡️🏃‍♂️‍➡️`);
         });
     })
     .catch((err) => {
